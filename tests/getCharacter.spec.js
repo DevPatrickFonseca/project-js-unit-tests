42107b8c79a90c8a5aed14c83c24f9d7d7954589
pfonsecafv <pfonsecafv@gmail.com>
2022-11-26 15:32:46 -0300

    Requisito 09.1 - Concluído

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -44,7 +44,7 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // 1. Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // 2. Teste se a função retorna o objeto correto para o parâmetro 'Arya',
@@ -53,4 +53,27 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
     // 5. Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
     // 6. Teste se ao passar um nome que não está na tabela, a função retorna undefined.
   });
+  it('01 - Se função não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBeUndefined();
+  });
+  
+  // it('02 - Se função retorna o objeto correto para o parâmetro "Arya"', () => {
+    
+  // });
+
+  // it('03 - Se função retorna o objeto correto para o parâmetro "Brienne"', () => {
+    
+  // });
+
+  // it('04 - Se função retorna o objeto correto para o parâmetro "Melissandre"', () => {
+    
+  // });
+  
+  // it('05 - Teste se o parâmetro não é Case Sensitive, retorna o mesmo objeto relativo a ele', () => {
+    
+  // });
+
+  // it('06 - Se ao passar um nome que não está na tabela, a função retorna undefined', () => {
+    
+  // });
 });
