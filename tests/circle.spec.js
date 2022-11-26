8654553b03e5c6567563c7a0cd03236da20f97d3
pfonsecafv <pfonsecafv@gmail.com>
2022-11-26 14:43:12 -0300

    Requisito 04 - Concluído Refatorado

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -24,15 +24,7 @@ const circle = require('../src/circle');
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
     // fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado possui 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
-    // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
-    
+    // ESCREVA SEUS TESTES ABAIXO:    
   });
   it("01 - Verifica se circle retorna undefined, se parâmetro passado não seja um número.", () => {
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
